70ae278831198bbce3ba66384dfe035d31a875db
fgabr <f.gabriele1604@gmail.com>
2025-12-24 00:52:52 +0100

    Improved player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,162 @@
 using System;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float movementSpeed = 3f;
-    public float jumpForce = 5f;
-    public float groundCheckRadius = 0.2f;
+    [Header(("Movement Settings"))] 
+    public float moveSpeed = 8f;
+    public float jumpForce = 16f;
+    [Range(0, 1)] public float groundDamping = 0.1f;
+    
+    [Header("Game Feel Settings")]
+    public float coyoteTime = 0.2f;
+    public float jumpBufferTime = 0.2f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    
+    [Header("Checks")]
     public Transform groundCheck;
-    public LayerMask groundMask;
-    
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private float horizontalInput;
     private bool isGrounded;
-    private Rigidbody2D rBody2D;
+    private bool facingRight = true;
+    private Vector3 velocity = Vector3.zero;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Timers
+    private float coyoteTimeCounter;
+    private float jumpBufferCounter;
+
+    private void Awake()
     {
-        rBody2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        rBody2D.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rBody2D.linearVelocity.y);
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
+        bool wasGrounded = isGrounded;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded)
         {
-            rBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            coyoteTimeCounter = coyoteTime;
         }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
+        {
+            PerformJump();
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 1f);
+            coyoteTimeCounter = 0f;
+        }
+
+        FlipCharacter();
     }
 
     private void FixedUpdate()
     {
-        isGrounded =  Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
+        Move(horizontalInput);
+        ApplyBetterGravity();
     }
+
+    void Move(float move)
+    {
+        Vector3 targetVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, groundDamping);
+    }
+
+    void PerformJump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        
+        jumpBufferCounter = 0f;
+    }
+
+    void ApplyBetterGravity()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.gravityScale = fallMultiplier;
+        } else if (rb.linearVelocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.gravityScale = lowJumpMultiplier;
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+    }
+
+    void FlipCharacter()
+    {
+        if (horizontalInput > 0 && !facingRight)
+        {
+            Flip();
+        } else if (horizontalInput < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+    // public float movementSpeed = 3f;
+    // public float jumpForce = 5f;
+    // public float groundCheckRadius = 0.2f;
+    // public Transform groundCheck;
+    // public LayerMask groundMask;
+    //
+    // private bool isGrounded;
+    // private Rigidbody2D rBody2D;
+    //
+    // // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+    //     rBody2D = GetComponent<Rigidbody2D>();
+    // }
+    //
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     rBody2D.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rBody2D.linearVelocity.y);
+    //
+    //     if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
+    //     {
+    //         rBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    //     }
+    // }
+    //
+    // private void FixedUpdate()
+    // {
+    //     isGrounded =  Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
+    // }
 }
